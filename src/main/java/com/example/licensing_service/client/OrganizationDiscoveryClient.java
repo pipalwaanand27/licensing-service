63c3c6b0ef93a182c2f2943e6e3c8dbe82502e8d
Anand Pipalwa <pipalwaanand@gmail.com>
2025-05-10 20:34:56 +0530

    Added circuit break resiliency configuration code

--- src/main/java/com/example/licensing_service/client/OrganizationDiscoveryClient.java
+++ src/main/java/com/example/licensing_service/client/OrganizationDiscoveryClient.java
@@ -1,6 +1,9 @@
 package com.example.licensing_service.client;
 
 import com.example.licensing_service.model.Organization;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,11 +15,14 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Component
+@Log4j2
 public class OrganizationDiscoveryClient {
 
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @CircuitBreaker(name = "organizationService", fallbackMethod = "getOrganizationServiceFallback")
+    @Retry(name = "organizationService")
     public Organization getOrganization(final String organizationId) {
         final RestTemplate restTemplate = new RestTemplate();
         final List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("organizationservice");
@@ -29,6 +35,18 @@ public class OrganizationDiscoveryClient {
         ResponseEntity<Organization> response =
                 restTemplate.exchange(serviceUrl, HttpMethod.GET, null, Organization.class, organizationId);
         return response.getBody();
+    }
+
+    public Organization getOrganizationServiceFallback(final String organizationId, final Throwable throwable) {
+
+        log.error("Fallback triggered due to: {}", throwable.getMessage());
+        final Organization organization = new Organization();
+        organization.setId(organizationId);
+        organization.setName("Default Organization");
+        organization.setContactEmail("orgdefault@gmail.com");
+        organization.setContactName("Default contact name");
+        organization.setContactPhone("9876543210");
+        return organization;
     }
 
 }
